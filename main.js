4b00f4cf2100e4e744fac4e9af0737b2f3903454
Margedt <margot@mirahi.io>
2023-04-04 21:40:05 +0200

    navigation

---
+++ main.js
@@ -0,0 +1,13 @@
+const hamb = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+let open;
+
+function openMenu() {
+    if (open) {
+       menu.style.display = "none";
+       open = false;
+    } else if (!open) {
+       menu.style.display = "block";
+       open = true;
+    }
+  }
